4c30b287db1ce6d9014a86b2d87c7ad4f4403ec9
Miso Korkiakoski <miso@Misos-MBP.koti.net>
2015-02-25 20:31:32 +0200

    add cat years project

--- cat years/cat years/ViewController.swift
+++ cat years/cat years/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var ageInput: UITextField!
+    @IBAction func ageButton(sender: AnyObject) {
+        var age = ageInput.text.toInt()
+        if age != nil {
+            var catAge = countAge(age!)
+            ageLabel.text = "Your cat is \(catAge) years old"
+        } else {
+            ageLabel.text = "Enter your cat's age in full years"
+        }
+    }
+    
+    func countAge(age: Int) -> Int {
+        if (age == 0) { return 0 }
+        if (age == 1) { return 15 }
+        if (age == 2) { return 25 }
+        return ((age-2) * 4) + 25
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ageLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
